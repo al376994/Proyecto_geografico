b79fd89773cbf5df870db232aba5d42558781664
al376994 <al376994@uji.es>
2021-11-04 15:12:22 +0100

    Añadidas anotaciones en la clase APIHelper para explicar su funcionamiento

--- src/main/java/Geografico/model/API/APIHelper.java
+++ src/main/java/Geografico/model/API/APIHelper.java
@@ -6,18 +6,26 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class APIHelper {
-	public static final String airVisualKey = "bf324fc3-3c44-4f50-bd19-836179b019b8";
-	public static final String geoCodeKey = "136807723723026938676x50228";
+class APIHelper {
+	// Listado do keys para las APIs
+	static final String airVisualKey = "bf324fc3-3c44-4f50-bd19-836179b019b8";
+	static final String geoCodeKey = "136807723723026938676x50228";
+	
 	private static final HttpClient client = HttpClient.newHttpClient();
 
-	public static String getBody(String givenURI) {
+	// Esta funcion crea los recursos necesarios para hacer una peticion GET a una URL
+	// y devuelve el contenido en una String en json
+	// De normal solo hay que usar esta funcion desde fuera
+	static String getBody(String givenURI) {
 		HttpRequest request = makeRequest(givenURI);
 		if (request == null) return null;
 		HttpResponse<String> response = getResponse(request);
 		return response.body();
 	}
 /*
+	// Esta funcion hace lo mismo que la anterior pero hace una peticion PUT, algunas
+	// APIs lo hace asi, de momento se han encontrado formas de usar solo GET ya que
+	// esta funcion devuelve xml en vez de json por algun motivo
 	public static String getBody(String givenURI, String body) {
 		HttpRequest request = makePost(givenURI, body);
 		if (request == null) return null;
@@ -25,12 +33,16 @@ public class APIHelper {
 		return response.body();
 	}
 */
+	// Esta funcion crea una peticion GET a partir de una URL
 	static private HttpRequest makeRequest(String givenURI) {
 		return HttpRequest.newBuilder(URI.create(givenURI))
 				.header("accept", "application/json")
 				.build();
 	}
 
+	// Esta funcion crea una peticion PUT a partir de una URL, es posible que el
+	// problema de que devuelva sml en vez de json resida en esta funcion
+	// nadie la usa de momento
 	static private HttpRequest makePost(String givenURI, String body) {
 		return HttpRequest.newBuilder(URI.create(givenURI))
 				.header("Accept", "application/json")
@@ -39,6 +51,7 @@ public class APIHelper {
 				.build();
 	}
 
+	// Esta funcion hace la peticion y recive la respesta
 	static private HttpResponse<String> getResponse(HttpRequest request) {
 		HttpResponse<String> response = null;
 		try {
