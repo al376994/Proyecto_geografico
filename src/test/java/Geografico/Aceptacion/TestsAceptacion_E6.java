3ccb07af880b46bf847ecaa7edb8b5b584536224
al376994 <al376994@uji.es>
2021-11-05 18:45:30 +0100

    Quitada la anotación que hacia que spring lanzase la aplicación web cuando no hace falta para el Test E6

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_E6.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_E6.java
@@ -5,6 +5,7 @@ import Geografico.model.API.APIGeocodeInterface;
 import Geografico.model.Coordenadas;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
