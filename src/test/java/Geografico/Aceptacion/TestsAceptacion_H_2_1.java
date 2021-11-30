96f1e73157ce584734d7020a931bf6c57765c75b
al376994 <al376994@uji.es>
2021-11-30 04:11:50 +0100

    Terminado TestsAceptacion_H_2_1, faltaba darle nombre al usuario y inicializarlo en general, lo cual se habia colado en algunas partes de labase de datos y a causado problemas, cuidado, revisar como asegurar la integridad de la base de datos

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_2_1.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_2_1.java
@@ -1,8 +1,12 @@
 package Geografico.Aceptacion;
 
+import Geografico.model.API.APIGeocode;
+import Geografico.model.API.APIGeocodeInterface;
+import Geografico.model.ListaUsuario;
 import Geografico.model.Ubicacion;
 import Geografico.model.Usuario;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,10 +19,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestsAceptacion_H_2_1 {
 	private Usuario usuario;
+	private ListaUsuario listaUsuario;
 
 	@BeforeEach
-	public void setUp() {
+	public void setUp() throws SQLException {
+		listaUsuario = new ListaUsuario();
 		usuario = new Usuario();
+		usuario.setNombre("usuarioPruebas");
+		limpiarBaseDeDatos();
+		listaUsuario.addUsuario(usuario);
+		// Esto, aunque redundante, sirve para simular el comportamiento completo del programa
+		usuario = listaUsuario.getUsuario(usuario.getNombre(), null);
+		usuario.addAPIGeocode(new APIGeocode());
+	}
+
+	@AfterEach
+	private void limpiarBaseDeDatos() throws SQLException {
+		if (listaUsuario.getUsuario(usuario.getNombre(), null) != null) {
+			listaUsuario.deleteUsuario(usuario.getNombre(), null);
+		}
 	}
 
 	@Test
