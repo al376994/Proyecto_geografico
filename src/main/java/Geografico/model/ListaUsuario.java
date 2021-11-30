ef73ee4bc91f3834a5e04d805c0b88cfef7203dd
al376994 <al376994@uji.es>
2021-11-30 01:46:10 +0100

    Añadida la funcionlaidad para hacer get y delete de usuarios

--- src/main/java/Geografico/model/ListaUsuario.java
+++ src/main/java/Geografico/model/ListaUsuario.java
@@ -5,10 +5,13 @@ import java.sql.SQLException;
 public class ListaUsuario {
 	private DataBaseFunctions dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
 
-	public Usuario getUsuario(String nombre){
-		return null;
+	public Usuario getUsuario(String nombre, String contraseña) throws SQLException {
+		return dataBaseFunctions.getUsuario(nombre, contraseña);
 	}
 	public void addUsuario(Usuario usuario) throws SQLException {
 		dataBaseFunctions.addUsuario(usuario);
+	}
+	public void deleteUsuario(String nombre, String contraseña) throws SQLException {
+		dataBaseFunctions.deleteUsuario(nombre, contraseña);
 	}
 }
