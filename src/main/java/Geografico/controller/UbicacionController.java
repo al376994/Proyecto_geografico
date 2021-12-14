c5a337a0d3140ff7d17aa210a8a6170942226af7
al376994 <al376994@uji.es>
2021-12-14 04:22:58 +0100

    Comenzada visualizacion de la lista de la ubicaciones en ubicaciones.html

--- src/main/java/Geografico/controller/UbicacionController.java
+++ src/main/java/Geografico/controller/UbicacionController.java
@@ -1,6 +1,9 @@
 package Geografico.controller;
 
+import Geografico.model.DataBaseConnector;
+import Geografico.model.DataBaseFunctions;
 import Geografico.model.Ubicacion;
+import Geografico.model.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,15 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.util.List;
 
 @Controller
 @RequestMapping(value="/ubicaciones")
 public class UbicacionController {
 
     @RequestMapping(value="/lista")
-    public String redirigirUbicacion(Model model, HttpSession session){
+    public String redirigirUbicacion(Model model, HttpSession session) throws SQLException {
         if(ControllerFunctions.checkIsLogged(model, session, "/ubicaciones/lista")) return "redirect:/login";
         model.addAttribute("Ubicacion", new Ubicacion(0,0, ""));
+        List<Ubicacion> ubicaciones = ((Usuario)session.getAttribute("user")).getUbicaciones();
+        model.addAttribute("ubicaciones", ubicaciones);
         return "principal/ubicaciones";
     }
 
