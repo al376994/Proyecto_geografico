6d8c77efab8aed49b2a6e54f3d20c0c93627bdef
al376994 <al376994@uji.es>
2021-12-28 01:09:59 +0100

    Añadida funcion que controla el toggle de la propiedad "favorito" de una ubicacion

--- src/main/java/Geografico/controller/UbicacionController.java
+++ src/main/java/Geografico/controller/UbicacionController.java
@@ -19,76 +19,82 @@ import java.util.List;
 @RequestMapping(value="/ubicaciones")
 public class UbicacionController {
 
-    private final DataBaseFunctions dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
-    private final APIGeocode apiGeocode = new APIGeocode();
+	private final DataBaseFunctions dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
+	private final APIGeocode apiGeocode = new APIGeocode();
 
-    @RequestMapping(value="/lista")
-    public String redirigirUbicacion(Model model, HttpSession session) throws SQLException {
-        if(ControllerFunctions.checkIsLogged(model, session, "/ubicaciones/lista")) return "redirect:/login";
-        if (session.getAttribute("lastUbicacion") == null){
-            session.setAttribute("lastUbicacion", new Ubicacion(0,0, ""));
-            session.removeAttribute("coordenadasValidas");
-            session.removeAttribute("toponimoValido");
-        }
-        model.addAttribute("Ubicacion", session.getAttribute("lastUbicacion"));
-        List<Ubicacion> ubicaciones = ((Usuario)session.getAttribute("user")).getUbicaciones();
-        model.addAttribute("ubicaciones", ubicaciones);
-        model.addAttribute("coordenadasValidas", session.getAttribute("coordenadasValidas"));
-        model.addAttribute("toponimoValido", session.getAttribute("toponimoValido"));
-        return "principal/ubicaciones";
-    }
+	@RequestMapping(value="/lista")
+	public String redirigirUbicacion(Model model, HttpSession session) throws SQLException {
+		if(ControllerFunctions.checkIsLogged(model, session, "/ubicaciones/lista")) return "redirect:/login";
+		if (session.getAttribute("lastUbicacion") == null){
+			session.setAttribute("lastUbicacion", new Ubicacion(0,0, ""));
+			session.removeAttribute("coordenadasValidas");
+			session.removeAttribute("toponimoValido");
+		}
+		model.addAttribute("Ubicacion", session.getAttribute("lastUbicacion"));
+		List<Ubicacion> ubicaciones = ((Usuario)session.getAttribute("user")).getUbicaciones();
+		model.addAttribute("ubicaciones", ubicaciones);
+		model.addAttribute("coordenadasValidas", session.getAttribute("coordenadasValidas"));
+		model.addAttribute("toponimoValido", session.getAttribute("toponimoValido"));
+		return "principal/ubicaciones";
+	}
 
-    @RequestMapping(value = "/añadir", method = RequestMethod.POST, params = "action=validarCoordenadas")
-    public String validarCoordenadas(HttpSession session, @ModelAttribute("Ubicacion") Ubicacion ubicacion){
-        ubicacion.setNombre( ((Ubicacion)session.getAttribute("lastUbicacion")).getNombre() );
-        boolean coordenadasValidas = apiGeocode.validarCoordenadas(ubicacion.getLatitud(), ubicacion.getLongitud());
-        System.out.println("Validar Coordenadas: " + coordenadasValidas);
-        session.setAttribute("coordenadasValidas", coordenadasValidas);
-        session.setAttribute("lastUbicacion", ubicacion);
-        return "redirect:/ubicaciones/lista";
-    }
+	@RequestMapping(value = "/añadir", method = RequestMethod.POST, params = "action=validarCoordenadas")
+	public String validarCoordenadas(HttpSession session, @ModelAttribute("Ubicacion") Ubicacion ubicacion){
+		ubicacion.setNombre( ((Ubicacion)session.getAttribute("lastUbicacion")).getNombre() );
+		boolean coordenadasValidas = apiGeocode.validarCoordenadas(ubicacion.getLatitud(), ubicacion.getLongitud());
+		System.out.println("Validar Coordenadas: " + coordenadasValidas);
+		session.setAttribute("coordenadasValidas", coordenadasValidas);
+		session.setAttribute("lastUbicacion", ubicacion);
+		return "redirect:/ubicaciones/lista";
+	}
 
-    @RequestMapping(value = "/añadir", method = RequestMethod.POST, params = "action=validarToponimo")
-    public String validarToponimo(HttpSession session, @ModelAttribute("Ubicacion") Ubicacion ubicacion){
-        ubicacion.setLatitud( ((Ubicacion)session.getAttribute("lastUbicacion")).getLatitud() );
-        ubicacion.setLongitud( ((Ubicacion)session.getAttribute("lastUbicacion")).getLongitud() );
-        boolean toponimoValido = apiGeocode.validarToponimo(ubicacion.getNombre());
-        System.out.println("Validar Toponimo: " + toponimoValido);
-        session.setAttribute("toponimoValido", toponimoValido);
-        session.setAttribute("lastUbicacion", ubicacion);
-        return "redirect:/ubicaciones/lista";
-    }
+	@RequestMapping(value = "/añadir", method = RequestMethod.POST, params = "action=validarToponimo")
+	public String validarToponimo(HttpSession session, @ModelAttribute("Ubicacion") Ubicacion ubicacion){
+		ubicacion.setLatitud( ((Ubicacion)session.getAttribute("lastUbicacion")).getLatitud() );
+		ubicacion.setLongitud( ((Ubicacion)session.getAttribute("lastUbicacion")).getLongitud() );
+		boolean toponimoValido = apiGeocode.validarToponimo(ubicacion.getNombre());
+		System.out.println("Validar Toponimo: " + toponimoValido);
+		session.setAttribute("toponimoValido", toponimoValido);
+		session.setAttribute("lastUbicacion", ubicacion);
+		return "redirect:/ubicaciones/lista";
+	}
 
-    @RequestMapping(value = "/añadir", method = RequestMethod.POST, params = "action=añadirPorCoordenadas")
-    public String addUbicacionPorCoordenadas(HttpSession session, @ModelAttribute("Ubicacion") Ubicacion ubicacion) {
-        Usuario usuario = (Usuario) session.getAttribute("user");
-        try {
-            usuario.altaUbicacionCoordenadas(ubicacion.getLatitud(), ubicacion.getLongitud());
-        } catch (CoordenadasExcepcion e) {
-            e.printStackTrace();
-        } catch (AlreadyHasPlaceException e) {
-            e.printStackTrace();
-        }
-        return "redirect:/ubicaciones/lista";
-    }
+	@RequestMapping(value = "/añadir", method = RequestMethod.POST, params = "action=añadirPorCoordenadas")
+	public String addUbicacionPorCoordenadas(HttpSession session, @ModelAttribute("Ubicacion") Ubicacion ubicacion) {
+		Usuario usuario = (Usuario) session.getAttribute("user");
+		try {
+			usuario.altaUbicacionCoordenadas(ubicacion.getLatitud(), ubicacion.getLongitud());
+		} catch (CoordenadasExcepcion e) {
+			e.printStackTrace();
+		} catch (AlreadyHasPlaceException e) {
+			e.printStackTrace();
+		}
+		return "redirect:/ubicaciones/lista";
+	}
 
-    @RequestMapping(value = "/añadir", method = RequestMethod.POST, params = "action=añadirPorToponimo")
-    public String addUbicacionPorToponimo(HttpSession session, @ModelAttribute("Ubicacion") Ubicacion ubicacion){
-        Usuario usuario = (Usuario) session.getAttribute("user");
-        try {
-            usuario.altaUbicacionToponimo(ubicacion.getNombre());
-        } catch (AlreadyHasPlaceException e) {
-            e.printStackTrace();
-        }
-        return "redirect:/ubicaciones/lista";
-    }
+	@RequestMapping(value = "/añadir", method = RequestMethod.POST, params = "action=añadirPorToponimo")
+	public String addUbicacionPorToponimo(HttpSession session, @ModelAttribute("Ubicacion") Ubicacion ubicacion){
+		Usuario usuario = (Usuario) session.getAttribute("user");
+		try {
+			usuario.altaUbicacionToponimo(ubicacion.getNombre());
+		} catch (AlreadyHasPlaceException e) {
+			e.printStackTrace();
+		}
+		return "redirect:/ubicaciones/lista";
+	}
 
-    @RequestMapping(value = "/eliminar/{toponimo}")
-    public String deleteUbicacion(@SessionAttribute("user") Usuario usuario, @PathVariable String toponimo) {
-        Ubicacion ubicacion = usuario.getUbicacion(toponimo);
-        usuario.darDeBajaUbicacion(ubicacion);
-        return "redirect:/ubicaciones/lista";
-    }
+	@RequestMapping(value = "/toggleFavorito/{toponimo}")
+	public String toggleFavoritoUbicacion(@SessionAttribute("user") Usuario usuario, @PathVariable String toponimo) throws SQLException {
+		Ubicacion ubicacion = usuario.getUbicacion(toponimo);
+		usuario.toggleFavoritoUbicacion(ubicacion);
+		System.out.println("\n\n" + usuario.getUbicacion(toponimo).isFavorito() + "\n\n");
+		return "redirect:/ubicaciones/lista";
+	}
 
-
+	@RequestMapping(value = "/eliminar/{toponimo}")
+	public String deleteUbicacion(@SessionAttribute("user") Usuario usuario, @PathVariable String toponimo) {
+		Ubicacion ubicacion = usuario.getUbicacion(toponimo);
+		usuario.darDeBajaUbicacion(ubicacion);
+		return "redirect:/ubicaciones/lista";
+	}
 }
