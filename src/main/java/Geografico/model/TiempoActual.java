08a9cb2a004b42e535810a3e3202a0c4ad20d2be
al376994 <al376994@uji.es>
2021-12-29 02:03:23 +0100

    Añadida funcion que evalua la presion y devuelve esta evaluacion como presion baja (borrasca), presion alta (anticiclon) o presion media.

--- src/main/java/Geografico/model/TiempoActual.java
+++ src/main/java/Geografico/model/TiempoActual.java
@@ -1,68 +1,73 @@
 package Geografico.model;
 
 public class TiempoActual {
-    private String tActual;
-    private String sensacionTermica;
-    private String humedad;
-    private String presion;
-    private String descripcion;
+	private String tActual;
+	private String sensacionTermica;
+	private String humedad;
+	private String presion;
+	private String descripcion;
 
-    public TiempoActual(String tActual, String sensacionTermica, String humedad, String presion, String d) {
-        this.tActual = tActual;
-        this.sensacionTermica = sensacionTermica;
-        this.humedad = humedad;
-        this.presion = presion;
-        this.descripcion = d;
-    }
+	public TiempoActual(String tActual, String sensacionTermica, String humedad, String presion, String d) {
+		this.tActual = tActual;
+		this.sensacionTermica = sensacionTermica;
+		this.humedad = humedad;
+		this.presion = presion;
+		this.descripcion = d;
+	}
 
-    public String getDescripcion() {
-        return descripcion;
-    }
+	public String getDescripcion() {
+		return descripcion;
+	}
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 
-    public String gettActual() {
-        return tActual;
-    }
+	public String gettActual() {
+		return tActual;
+	}
 
-    public void settActual(String tActual) {
-        this.tActual = tActual;
-    }
+	public void settActual(String tActual) {
+		this.tActual = tActual;
+	}
 
-    public String getSensacionTermica() {
-        return sensacionTermica;
-    }
+	public String getSensacionTermica() {
+		return sensacionTermica;
+	}
 
-    public void setSensacionTermica(String sensacionTermica) {
-        this.sensacionTermica = sensacionTermica;
-    }
+	public void setSensacionTermica(String sensacionTermica) {
+		this.sensacionTermica = sensacionTermica;
+	}
 
-    public String getHumedad() {
-        return humedad;
-    }
+	public String getHumedad() {
+		return humedad;
+	}
 
-    public void setHumedad(String humedad) {
-        this.humedad = humedad;
-    }
+	public void setHumedad(String humedad) {
+		this.humedad = humedad;
+	}
 
-    public String getPresion() {
-        return presion;
-    }
+	public String getPresion() {
+		return presion;
+	}
 
-    public void setPresion(String presion) {
-        this.presion = presion;
-    }
+	public String getEvaluacionDePresion() {
+		return Integer.parseInt(presion)<1013 ? "Presion baja" :
+				Integer.parseInt(presion)>1013 ? "Presion alta" : "Presion media";
+	}
 
-    @Override
-    public String toString() {
-        return "TiempoActual{" +
-                "tActual='" + tActual + '\'' +
-                ", sensacionTermica='" + sensacionTermica + '\'' +
-                ", humedad='" + humedad + '\'' +
-                ", presion='" + presion + '\'' +
-                ", descripcion='" + descripcion + '\'' +
-                '}';
-    }
+	public void setPresion(String presion) {
+		this.presion = presion;
+	}
+
+	@Override
+	public String toString() {
+		return "TiempoActual{" +
+				"tActual='" + tActual + '\'' +
+				", sensacionTermica='" + sensacionTermica + '\'' +
+				", humedad='" + humedad + '\'' +
+				", presion='" + presion + '\'' +
+				", descripcion='" + descripcion + '\'' +
+				'}';
+	}
 }
