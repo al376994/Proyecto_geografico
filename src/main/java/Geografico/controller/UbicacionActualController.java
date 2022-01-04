830bdbf5bb7ecd8d7e34755096b0d07b652b7d66
AlejandroGB13 <al386123@uji.es>
2022-01-04 18:39:05 +0100

    Tiempo ubicación actual

---
+++ src/main/java/Geografico/controller/UbicacionActualController.java
@@ -0,0 +1,35 @@
+package Geografico.controller;
+
+import Geografico.model.*;
+import Geografico.model.API.APIAirVisual;
+import Geografico.model.API.APIGeocode;
+import Geografico.model.API.APIOpenDataSoft;
+import Geografico.model.API.APIOpenWeather;
+import org.json.JSONException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+
+@Controller
+@RequestMapping(value="/ubicacionActual")
+public class UbicacionActualController {
+    private final DataBaseFunctions dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
+    private final APIAirVisual apiAirVisual = new APIAirVisual();
+    private final APIOpenWeather apiOpenWeather = new APIOpenWeather();
+    private final APIOpenDataSoft apiOpenDataSoft = new APIOpenDataSoft();
+
+    @RequestMapping(value="/servicios")
+    public String redirigirServicio(Model model, HttpSession session) throws SQLException, JSONException {
+        Ciudad ciudad = apiAirVisual.getCiudadCercana();
+        Ciudad aux = apiOpenDataSoft.cambiarParaOpenWeather(ciudad);
+        Polucion polucion = apiAirVisual.getPolucionCiudadCercana();
+        TiempoActual tiempoActual = apiOpenWeather.getTiempoActual(aux);
+        model.addAttribute("ciudad",ciudad);
+        model.addAttribute("polucion",polucion);
+        model.addAttribute("tiempo",tiempoActual);
+        return "principal/ubicacionActual";
+    }
+}
