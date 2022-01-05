8dcf38c33df7382e1e3af4ca0d7bd4a6058dba93
al376994 <al376994@uji.es>
2022-01-05 23:10:57 +0100

    Pequeño arreglo en el Arrange del test de aceptación H2_3. Añadido además Before and After que crean un usuario completo y despues lo eliminan para mantener la BBDD limpia.

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_2_3.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_2_3.java
@@ -1,21 +1,43 @@
 package Geografico.Aceptacion;
 
+import Geografico.model.API.APIGeocode;
+import Geografico.model.ListaUsuario;
 import Geografico.model.Ubicacion;
 import Geografico.model.Usuario;
+import Geografico.model.excepciones.AlreadyHasPlaceException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestsAceptacion_H_2_3 {
     private Usuario usuario;
+    private Ubicacion ubicacion;
+    private ListaUsuario listaUsuario;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws SQLException, AlreadyHasPlaceException {
+        listaUsuario = new ListaUsuario();
         usuario = new Usuario();
+        usuario.setNombre("userP");
+        usuario.setContrasena("passP");
+        limpiarBaseDeDatos();
+        listaUsuario.addUsuario(usuario);
+        // Esto, aunque redundante, sirve para simular el comportamiento completo del programa
+        usuario = listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena());
+        usuario.addAPIGeocode(new APIGeocode());
+        ubicacion = usuario.altaUbicacionToponimo("Castellón");
+    }
+
+    @AfterEach
+    private void limpiarBaseDeDatos() throws SQLException {
+        if (listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena()) != null) {
+            listaUsuario.deleteUsuario(usuario.getNombre(), usuario.getContrasena());
+        }
     }
 
     @Test
@@ -30,6 +52,7 @@ public class TestsAceptacion_H_2_3 {
     @Test
     public void consultarListaUbicacionesActivas_E2_3_2_noHayUbicacionesEnLaLista() throws SQLException {
         //Arrange
+        usuario.darDeBajaUbicacion(ubicacion);
         //Act
         List<Ubicacion> ubicacionesActivas = usuario.getUbicacionesActivas();
         //Assert
