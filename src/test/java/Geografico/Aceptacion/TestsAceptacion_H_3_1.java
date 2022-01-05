52a3d4fcc9734d8e864afdc60ff06c459cff5252
al376994 <al376994@uji.es>
2022-01-06 00:24:03 +0100

    Ahora el test de aceptacion H_3_1 es independiente de los servicios reales de la aplicación, ya que estos no se pueden borrar de la BBDD y borrarlos es la unica forma de representar esta prueba.

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_3_1.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_3_1.java
@@ -1,45 +1,63 @@
 package Geografico.Aceptacion;
 
+import Geografico.model.API.APIGeocode;
 import Geografico.model.DataBaseConnector;
 import Geografico.model.DataBaseFunctions;
+import Geografico.model.ListaUsuario;
 import Geografico.model.Usuario;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.sql.Connection;
-import java.util.List;
+import java.sql.SQLException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TestsAceptacion_H_3_1 {
+	DataBaseFunctions dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
+
+	private Usuario usuario;
+	private ListaUsuario listaUsuario;
+
+	@BeforeEach
+	public void setUp() throws SQLException {
+		listaUsuario = new ListaUsuario();
+		usuario = new Usuario();
+		usuario.setNombre("userP");
+		usuario.setContrasena("passP");
+		limpiarBaseDeDatos();
+		listaUsuario.addUsuario(usuario);
+		// Esto, aunque redundante, sirve para simular el comportamiento completo del programa
+		usuario = listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena());
+		usuario.addAPIGeocode(new APIGeocode());
+		dataBaseFunctions.addServicioAPIDisponible("ServicioImaginario");
+	}
+
+	@AfterEach
+	private void limpiarBaseDeDatos() throws SQLException {
+		if (listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena()) != null) {
+			listaUsuario.deleteUsuario(usuario.getNombre(), usuario.getContrasena());
+		}
+		dataBaseFunctions.deleteServicioAPIDisponible("ServicioImaginario");
+	}
 
     @Test
     public void consultarServiciosAPIDisponibles_E3_1_1_SeMuestranLosDisponibles() {
         //Arrange
-        Connection conn = DataBaseConnector.getConnection();
-        DataBaseFunctions dataBaseFunctions = new DataBaseFunctions(conn);
-
-        dataBaseFunctions.addServicioAPIDisponible("AirVisual");
-        dataBaseFunctions.addServicioAPIDisponible("Currents");
-
-        Usuario usuario = new Usuario("NuevoUser");
         //Act
-        List<String> APIsDisponibles= usuario.getServiciosAPIDisponibles();
-
-        dataBaseFunctions.deleteServicioAPIDisponible("AirVisual");
-        dataBaseFunctions.deleteServicioAPIDisponible("Currents");
-
+		boolean tieneServicio = usuario.getServiciosAPIDisponibles().contains("ServicioImaginario");
         //Assert
-        assertEquals(2, APIsDisponibles.size());
+		assertTrue(tieneServicio);
     }
 
     @Test
     public void consultarServiciosAPIDisponibles_E3_1_2_SeNotificaQueNoHayDisponibles() {
         //Arrange
-        Usuario usuario = new Usuario("NuevoUser");
+		dataBaseFunctions.deleteServicioAPIDisponible("ServicioImaginario");
         //Act
-        List<String> APIsDisponibles= usuario.getServiciosAPIDisponibles();
-        //Assert
-        assertEquals(0, APIsDisponibles.size());
+		boolean tieneServicio = usuario.getServiciosAPIDisponibles().contains("ServicioImaginario");
+		//Assert
+		assertFalse(tieneServicio);
     }
 }
