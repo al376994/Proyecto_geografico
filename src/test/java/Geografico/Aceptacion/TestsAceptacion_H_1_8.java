69e16076efdf36c69a8b9b674ea5eef3df1e7590
al376994 <al376994@uji.es>
2022-01-05 20:57:48 +0100

    Pequeño arreglo el Act y Assert del test de aceptación H1_8. Añadido además Before and After que crean un usuario completo y despues lo eliminan para mantener la BBDD limpia.

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_1_8.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_1_8.java
@@ -2,11 +2,11 @@ package Geografico.Aceptacion;
 
 import Geografico.model.API.APIGeocode;
 import Geografico.model.API.APIGeocodeInterface;
-import Geografico.model.Ubicacion;
+import Geografico.model.ListaUsuario;
 import Geografico.model.Usuario;
 import Geografico.model.excepciones.AlreadyHasPlaceException;
 import Geografico.model.excepciones.CoordenadasExcepcion;
-import org.junit.Rule;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,36 +20,48 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class TestsAceptacion_H_1_8 {
     private Usuario usuario;
     private APIGeocodeInterface apiGeocode;
+    private ListaUsuario listaUsuario;
 
 
     @BeforeEach
     private void iniciarVariables() throws SQLException {
         apiGeocode = new APIGeocode();
-        usuario = new Usuario();			//para hacer tests al principio creamos un usuario a mano
+        listaUsuario = new ListaUsuario();
+        usuario = new Usuario();
         usuario.setNombre("usuario5");
+        usuario.setContrasena("usuario5");
+        limpiarBaseDeDatos();
+        listaUsuario.addUsuario(usuario);
+        // Esto, aunque redundante, sirve para simular el comportamiento completo del programa
+        usuario = listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena());
+    }
+
+    @AfterEach
+    private void limpiarBaseDeDatos() throws SQLException {
+        if (listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena()) != null) {
+            listaUsuario.deleteUsuario(usuario.getNombre(), usuario.getContrasena());
+        }
     }
 
     @Test
-    public void asignarAliasUbicacion_E1_8_1_devuelveAliasCorrecto() throws SQLException, CoordenadasExcepcion, AlreadyHasPlaceException {
+    public void asignarAliasUbicacion_E1_8_1_devuelveAliasCorrecto() throws CoordenadasExcepcion, AlreadyHasPlaceException {
         //Arrange
         usuario.addAPIGeocode(apiGeocode);
         usuario.altaUbicacionCoordenadas(39.986,-0.0376709);
         //Act
-        usuario.asignarAliasUbicacion("Castellon de la Plana", "casa");
+        usuario.asignarAliasUbicacion("Castello De La Plana", "casa");
         //Assert
-        assertEquals(usuario.getAliasUbicacion("Castellon de la Plana"), "casa");
+        assertEquals("casa", usuario.getAliasUbicacion("Castello De La Plana"));
     }
     @Test
-    public void asignarAliasUbicacion_E1_8_2_devuelveAliasIncorrecto() throws SQLException, CoordenadasExcepcion, AlreadyHasPlaceException {
+    public void asignarAliasUbicacion_E1_8_2_devuelveAliasIncorrecto() throws CoordenadasExcepcion, AlreadyHasPlaceException {
         //Arrange
         usuario.addAPIGeocode(apiGeocode);
         usuario.altaUbicacionCoordenadas(39.986,-0.0376709);
         //Act
 
         assertThrows(IllegalArgumentException.class,
-                ()->{
-                    usuario.asignarAliasUbicacion("Castellon de la Plana", "");
-        });
+                ()-> usuario.asignarAliasUbicacion("Castello de la Plana", ""));
         //Assert
 //        assertEquals(usuario.getAliasUbicacion("Castellon de la Plana"), "casa");
     }
