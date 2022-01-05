9d207ccaece806b63a9f969bab0b1b5f9e5ec0c8
al376994 <al376994@uji.es>
2022-01-05 22:51:09 +0100

    Pequeño arreglo el Assert del test de aceptación H1_9. Añadido además Before and After que crean un usuario completo y despues lo eliminan para mantener la BBDD limpia.

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_1_9.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_1_9.java
@@ -1,9 +1,14 @@
 package Geografico.Aceptacion;
 
+import Geografico.model.ListaUsuario;
 import Geografico.model.Ubicacion;
 import Geografico.model.Usuario;
+import Geografico.model.excepciones.AlreadyHasPlaceException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,19 +16,33 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TestsAceptacion_H_1_9 {
     private Usuario usuario;
     private Ubicacion ubicacion;
+    private ListaUsuario listaUsuario;
 
     @BeforeEach
-    public void setUp() {
+    private void setUp() throws SQLException, AlreadyHasPlaceException {
+        listaUsuario = new ListaUsuario();
         usuario = new Usuario();
-        ubicacion = new Ubicacion(39.97990, -0.03304, "Castello De La Plana");
-        ubicacion.setAlias("Castellón");
+        usuario.setNombre("usuarioPruebas");
+        usuario.setContrasena("pwd");
+        limpiarBaseDeDatos();
+        listaUsuario.addUsuario(usuario);
+        usuario = listaUsuario.getUsuario(usuario.getNombre(), null);
+        ubicacion = usuario.altaUbicacionToponimo("Castellón");
+    }
+
+    @AfterEach
+    private void limpiarBaseDeDatos() throws SQLException {
+        if (listaUsuario.getUsuario(usuario.getNombre(), null) != null) {
+            listaUsuario.deleteUsuario(usuario.getNombre(), null);
+        }
     }
 
     @Test
     public void desactivarUbicacionActiva_E1_9_1_laUbicacionQuedaDesactivada() {
         //Arrange
+        //Las ubicaciones estan activadas por defecto
         //Act
-        boolean desactivada = usuario.desactivarUbicacion(ubicacion);
+        boolean desactivada = !usuario.desactivarUbicacion(ubicacion);
         //Assert
         assertTrue(desactivada);
     }
@@ -31,9 +50,10 @@ public class TestsAceptacion_H_1_9 {
     @Test
     public void desactivarUbicacionYaDesactivada_E1_9_2_laUbicacionPermaneceDesactivada() {
         //Arrange
+        usuario.desactivarUbicacion(ubicacion);
         //Act
-        boolean desactivada = usuario.desactivarUbicacion(ubicacion);
+        boolean desactivada = !usuario.desactivarUbicacion(ubicacion);
         //Assert
-        assertFalse(desactivada);
+        assertTrue(desactivada);
     }
 }
