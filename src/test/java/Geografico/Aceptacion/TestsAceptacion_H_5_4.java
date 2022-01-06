a76790eab7a25a6838783a50c2212f94eb05be22
al376994 <al376994@uji.es>
2022-01-06 02:11:47 +0100

    Corregida test de aceptacion H_5_4 que daba una excepcion ya que el usuario ya existia, añadida una funcion aftereach que borra al usuario tras cada test

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_5_4.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_5_4.java
@@ -2,8 +2,9 @@ package Geografico.Aceptacion;
 
 import Geografico.model.DataBaseConnector;
 import Geografico.model.DataBaseFunctions;
+import Geografico.model.ListaUsuario;
 import Geografico.model.Usuario;
-import Geografico.model.excepciones.CoordenadasExcepcion;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestsAceptacion_H_5_4 {
     private Usuario usuario;
     private DataBaseFunctions dataBaseFunctions;
+    private ListaUsuario listaUsuario;
 
 
     @BeforeEach
@@ -21,10 +23,19 @@ public class TestsAceptacion_H_5_4 {
         usuario = new Usuario();
         usuario.setNombre("usuarioCP");
         dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
+        listaUsuario = new ListaUsuario();
+        limpiarBaseDeDatos();
+    }
+
+    @AfterEach
+    private void limpiarBaseDeDatos() throws SQLException {
+        if (listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena()) != null) {
+            listaUsuario.deleteUsuario(usuario.getNombre(), usuario.getContrasena());
+        }
     }
 
     @Test
-    public void modificarPerfil_E5_4_1_modificadoCorrecto() throws SQLException, CoordenadasExcepcion {
+    public void modificarPerfil_E5_4_1_modificadoCorrecto() throws SQLException {
         //Arrange
         usuario.setContrasena("pwd");
         //Act
