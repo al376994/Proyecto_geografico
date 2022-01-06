cb7df89d15c82c75be843b17af9c0be7f4a7cb2b
al376994 <al376994@uji.es>
2022-01-06 01:55:08 +0100

    Corregida test de aceptacion H_5_1 que daba una excepcion ya que el tamaño maximo de las contraseñas es 10 y las usadas ateriormente tenian más de 10 caracteres.

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_5_1.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_5_1.java
@@ -1,52 +1,58 @@
 package Geografico.Aceptacion;
 
-import Geografico.model.API.APIGeocode;
-import Geografico.model.API.APIGeocodeInterface;
 import Geografico.model.DataBaseConnector;
 import Geografico.model.DataBaseFunctions;
-import Geografico.model.Ubicacion;
+import Geografico.model.ListaUsuario;
 import Geografico.model.Usuario;
 import Geografico.model.excepciones.CoordenadasExcepcion;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestsAceptacion_H_5_1 {
     private Usuario usuario;
     private DataBaseFunctions dataBaseFunctions;
-
+    private ListaUsuario listaUsuario;
 
     @BeforeEach
     private void iniciarVariables() throws SQLException {
         usuario = new Usuario();
         usuario.setNombre("nuevoUsuario");
         dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
+        listaUsuario = new ListaUsuario();
+        limpiarBaseDeDatos();
+    }
+
+    @AfterEach
+    private void limpiarBaseDeDatos() throws SQLException {
+        if (listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena()) != null) {
+            listaUsuario.deleteUsuario(usuario.getNombre(), usuario.getContrasena());
+        }
     }
 
     @Test
-    public void registrarUsuario_E5_1_1_registroCorrecto() throws SQLException, CoordenadasExcepcion {
+    public void registrarUsuario_E5_1_1_registroCorrecto() throws SQLException {
         //Arrange
-        usuario.setContrasena("superContrasenaSecreta");
+        usuario.setContrasena("pa$$word");
         //Act
         dataBaseFunctions.addUsuario(usuario);
         List<String> usuarios = dataBaseFunctions.listarUsuarios();
         //Assert
-        assertEquals(true, usuarios.contains("nuevoUsuario"));
+        assertTrue(usuarios.contains("nuevoUsuario"));
     }
 
     @Test
-    public void registrarUsuario_E5_1_2_registroIncorrecto() throws SQLException {
+    public void registrarUsuario_E5_1_2_registroIncorrecto() {
         //Arrange
         usuario.setContrasena("");
         //Act
         assertThrows(IllegalArgumentException.class,
-                ()->{
-                    dataBaseFunctions.addUsuario(usuario);
-        });
+                ()-> dataBaseFunctions.addUsuario(usuario));
     }
 }
