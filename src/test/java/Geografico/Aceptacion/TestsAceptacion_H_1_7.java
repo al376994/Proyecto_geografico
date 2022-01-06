b4e0e2eb668b435999a604e08d669acc020a9ad7
al376994 <al376994@uji.es>
2022-01-06 21:18:46 +0100

    Añadida adición y eliminación de usuario a la BBDD antes y despues de cada test.

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_1_7.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_1_7.java
@@ -2,38 +2,50 @@ package Geografico.Aceptacion;
 
 import Geografico.model.API.APIGeocode;
 import Geografico.model.API.APIGeocodeInterface;
-import Geografico.model.Coordenadas;
+import Geografico.model.ListaUsuario;
 import Geografico.model.Ubicacion;
 import Geografico.model.Usuario;
 import Geografico.model.excepciones.CoordenadasExcepcion;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.net.CookieHandler;
 import java.sql.SQLException;
-import java.util.List;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class TestsAceptacion_H_1_7 {
     private APIGeocodeInterface apiGeocode;
+    private ListaUsuario listaUsuario;
     private Usuario usuario;
+
 
 
     @BeforeEach
     private void iniciarVariables() throws SQLException {
         apiGeocode = new APIGeocode();
-        usuario = new Usuario();			//para hacer tests al principio creamos un usuario a mano
+        listaUsuario = new ListaUsuario();
+        usuario = new Usuario();
         usuario.setNombre("usuario5");
+        usuario.setContrasena("usuario5");
+        limpiarBaseDeDatos();
+        listaUsuario.addUsuario(usuario);
+        // Esto, aunque redundante, sirve para simular el comportamiento completo del programa
+        usuario = listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena());
+    }
+
+    @AfterEach
+    private void limpiarBaseDeDatos() throws SQLException {
+        if (listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena()) != null) {
+            listaUsuario.deleteUsuario(usuario.getNombre(), usuario.getContrasena());
+        }
     }
 
     @Test
-    public void obtenerToponimoCercanoCoordenadas_E1_7_1_devuelveToponimo() throws SQLException, CoordenadasExcepcion {
+    public void obtenerToponimoCercanoCoordenadas_E1_7_1_devuelveToponimo() throws CoordenadasExcepcion {
         //Arrange
         usuario.addAPIGeocode(apiGeocode);
         //Act
