33aa0b44d9a7a7cf2674cde1b0071803efff2cb0
al376994 <al376994@uji.es>
2022-01-07 00:31:55 +0100

    Ahora arreglada ya que tenia el problema de doble PK al intentar añadir un usuario que ya existita, puesto el mismo sistema de control que en las otras pruebas para evitarlo.

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_5_3.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_5_3.java
@@ -2,9 +2,11 @@ package Geografico.Aceptacion;
 
 import Geografico.model.DataBaseConnector;
 import Geografico.model.DataBaseFunctions;
-import Geografico.model.Ubicacion;
+import Geografico.model.ListaUsuario;
 import Geografico.model.Usuario;
-import Geografico.model.excepciones.CoordenadasExcepcion;
+import Geografico.model.Ubicacion;
+import Geografico.model.excepciones.AlreadyHasPlaceException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +14,12 @@ import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TestsAceptacion_H_5_3 {
     private Usuario usuario;
     private DataBaseFunctions dataBaseFunctions;
+    private ListaUsuario listaUsuario;
 
 
     @BeforeEach
@@ -23,16 +27,28 @@ public class TestsAceptacion_H_5_3 {
         usuario = new Usuario();
         usuario.setNombre("usuarioFv");
         dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
+        listaUsuario = new ListaUsuario();
+        usuario.setContrasena("pwd");
+        limpiarBaseDeDatos();
+        listaUsuario.addUsuario(usuario);
+        // Esto, aunque redundante, sirve para simular el comportamiento completo del programa
+        usuario = listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena());
+    }
+
+    @AfterEach
+    private void limpiarBaseDeDatos() throws SQLException {
+        if (listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena()) != null) {
+            listaUsuario.deleteUsuario(usuario.getNombre(), usuario.getContrasena());
+        }
     }
 
     @Test
-    public void cerrarSesion_E5_3_1_cierreCorrecto() throws SQLException, CoordenadasExcepcion {
+    public void cerrarSesion_E5_3_1_cierreCorrecto() throws SQLException, AlreadyHasPlaceException {
         //vamos a pedir primero las ubicaciones del usuario y luego simularemos un cierre de sesión
         //estableciendo la bbdd a null
         //Arrange
-        usuario.setContrasena("pwd");
+        usuario.altaUbicacionToponimo("Castellón");
         //Act
-        dataBaseFunctions.addUsuario(usuario);
         List<Ubicacion> ubicacionesActivas = dataBaseFunctions.listarUbicacionesUsuario(usuario.getNombre());
         String nombreUbicacion1 = ubicacionesActivas.get(0).getNombre();
         dataBaseFunctions = null;
@@ -41,15 +57,14 @@ public class TestsAceptacion_H_5_3 {
     }
 
     @Test
-    public void cerrarSesion_E5_3_2_cierreIncorrecto() throws SQLException {
+    public void cerrarSesion_E5_3_2_cierreIncorrecto() throws SQLException, AlreadyHasPlaceException {
         //Arrange
-        usuario.setContrasena("pwd");
+        usuario.altaUbicacionToponimo("Castellón");
         //Act
-        dataBaseFunctions.addUsuario(usuario);
         List<Ubicacion> ubicacionesActivas = dataBaseFunctions.listarUbicacionesUsuario(usuario.getNombre());
         String nombreUbicacion1 = ubicacionesActivas.get(0).getNombre();
         dataBaseFunctions = null;
         //Assert
-        assertEquals(null, dataBaseFunctions);
+        assertNull(dataBaseFunctions);
     }
 }
