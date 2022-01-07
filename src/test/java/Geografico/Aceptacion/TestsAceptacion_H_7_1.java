145260fb783e089c45db9271f9d8f0e0c7219bce
al376994 <al376994@uji.es>
2022-01-07 16:45:46 +0100

    Arreglados TI 7_1 y 7_2 que no creaban un usuario en la BBDD.

--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_7_1.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_7_1.java
@@ -1,21 +1,21 @@
 package Geografico.Aceptacion;
 
-import Geografico.model.DataBaseConnector;
-import Geografico.model.DataBaseFunctions;
-import Geografico.model.Ubicacion;
-import Geografico.model.Usuario;
+import Geografico.model.*;
+import Geografico.model.excepciones.AlreadyHasPlaceException;
 import Geografico.model.excepciones.CoordenadasExcepcion;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestsAceptacion_H_7_1 {
     private Usuario usuario;
     private DataBaseFunctions dataBaseFunctions;
+    private ListaUsuario listaUsuario;
 
 
     @BeforeEach
@@ -23,25 +23,39 @@ public class TestsAceptacion_H_7_1 {
         usuario = new Usuario();
         usuario.setNombre("usuarioFv");
         dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
+        listaUsuario = new ListaUsuario();
+        usuario.setContrasena("pwd");
+        limpiarBaseDeDatos();
+        listaUsuario.addUsuario(usuario);
+        // Esto, aunque redundante, sirve para simular el comportamiento completo del programa
+        usuario = listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena());
+    }
+
+    @AfterEach
+    private void limpiarBaseDeDatos() throws SQLException {
+        if (listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena()) != null) {
+            listaUsuario.deleteUsuario(usuario.getNombre(), usuario.getContrasena());
+        }
     }
 
     @Test
-    public void anadirLocacalizacionFavorita_E7_1_1_anadidoCorrecto() throws SQLException, CoordenadasExcepcion {
+    public void anadirLocacalizacionFavorita_E7_1_1_anadidoCorrecto() throws SQLException, AlreadyHasPlaceException {
         //Arrange
+        usuario.altaUbicacionToponimo("Castellón");
         List<Ubicacion> listaUbicaciones = dataBaseFunctions.listarUbicacionesUsuario(usuario.getNombre());
         Ubicacion castellon = listaUbicaciones.get(0);
         //Act
-        Boolean anadido = dataBaseFunctions.anadirUbicacionFavorita(usuario.getNombre(), castellon.getNombre());
+        boolean anadido = dataBaseFunctions.anadirUbicacionFavorita(usuario.getNombre(), castellon.getNombre());
         //Assert
-        assertEquals(true, anadido);
+        assertTrue(anadido);
     }
 
     @Test
     public void anadirLocacalizacionFavorita_E7_1_2_anadidoIncorrecto() throws SQLException {
         //Arrange
         //Act
-        Boolean anadido = dataBaseFunctions.anadirUbicacionFavorita(usuario.getNombre(), "sinNombre");
+        boolean anadido = dataBaseFunctions.anadirUbicacionFavorita(usuario.getNombre(), "sinNombre");
         //Assert
-        assertEquals(false, anadido);
+        assertFalse(anadido);
     }
 }
--- src/test/java/Geografico/Aceptacion/TestsAceptacion_H_7_2.java
+++ src/test/java/Geografico/Aceptacion/TestsAceptacion_H_7_2.java
@@ -1,21 +1,21 @@
 package Geografico.Aceptacion;
 
-import Geografico.model.DataBaseConnector;
-import Geografico.model.DataBaseFunctions;
-import Geografico.model.Ubicacion;
-import Geografico.model.Usuario;
-import Geografico.model.excepciones.CoordenadasExcepcion;
+import Geografico.model.*;
+import Geografico.model.excepciones.AlreadyHasPlaceException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestsAceptacion_H_7_2 {
     private Usuario usuario;
     private DataBaseFunctions dataBaseFunctions;
+    private ListaUsuario listaUsuario;
 
 
     @BeforeEach
@@ -23,28 +23,43 @@ public class TestsAceptacion_H_7_2 {
         usuario = new Usuario();
         usuario.setNombre("usuarioFv");
         dataBaseFunctions = new DataBaseFunctions(DataBaseConnector.getConnection());
+        listaUsuario = new ListaUsuario();
+        usuario.setContrasena("pwd");
+        limpiarBaseDeDatos();
+        listaUsuario.addUsuario(usuario);
+        // Esto, aunque redundante, sirve para simular el comportamiento completo del programa
+        usuario = listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena());
+    }
+
+    @AfterEach
+    private void limpiarBaseDeDatos() throws SQLException {
+        if (listaUsuario.getUsuario(usuario.getNombre(), usuario.getContrasena()) != null) {
+            listaUsuario.deleteUsuario(usuario.getNombre(), usuario.getContrasena());
+        }
     }
 
     @Test
-    public void desactivarLocacalizacionFavorita_E7_2_1_desactivadoCorrecto() throws SQLException, CoordenadasExcepcion {
+    public void desactivarLocacalizacionFavorita_E7_2_1_desactivadoCorrecto() throws SQLException, AlreadyHasPlaceException {
         //Arrange
+        usuario.altaUbicacionToponimo("Castellón");
         List<Ubicacion> listaUbicaciones = dataBaseFunctions.listarUbicacionesUsuario(usuario.getNombre());
         Ubicacion castellon = listaUbicaciones.get(0);
-
         //Act
-        Boolean anadido = dataBaseFunctions.desactivarUbicacionFavorita(usuario.getNombre(), castellon.getNombre());
+        usuario.toggleFavoritoUbicacion(castellon);
+        boolean anadido = dataBaseFunctions.desactivarUbicacionFavorita(usuario.getNombre(), castellon.getNombre());
         //Assert
-        assertEquals(true, anadido);
+        assertTrue(anadido);
     }
 
     @Test
-    public void desactivarLocacalizacionFavorita_E7_2_2_desactivadoIncorrecto() throws SQLException {
+    public void desactivarLocacalizacionFavorita_E7_2_2_desactivadoIncorrecto() throws SQLException, AlreadyHasPlaceException {
         //Arrange
+        Ubicacion ubicacion = usuario.altaUbicacionToponimo("Castellón");
         List<Ubicacion> listaUbicaciones = dataBaseFunctions.listarUbicacionesUsuario(usuario.getNombre());
         Ubicacion castellon = listaUbicaciones.get(0);
         //Act
-        Boolean anadido = dataBaseFunctions.desactivarUbicacionFavorita(usuario.getNombre(), castellon.getNombre());
+        boolean anadido = dataBaseFunctions.desactivarUbicacionFavorita(usuario.getNombre(), castellon.getNombre());
         //Assert
-        assertEquals(false, anadido);
+        assertFalse(anadido);
     }
 }
